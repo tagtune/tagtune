1f271f3529916a617b621e835e8d1a31ddaaf9d0
Youngwoo Choi <yw823x@daum.net>
2023-05-30 15:53:49 +0900

    Enhancement: SpotifyTokenTest

--- src/test/java/com/ll/tagtune/base/SpotifyTokenTest.java
+++ src/test/java/com/ll/tagtune/base/SpotifyTokenTest.java
@@ -1,16 +1,32 @@
 package com.ll.tagtune.base;
 
+import com.ll.tagtune.base.spotify.CreateToken;
 import com.ll.tagtune.base.spotify.MusicSearch;
+import com.ll.tagtune.boundedContext.music.entity.Music;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpotifyTokenTest {
     @Test
     @DisplayName("Spotify Access Token")
     void t001() throws Exception {
+        final String accessToken = CreateToken.getAccessToken();
+        System.out.println(accessToken);
+        assertThat(accessToken).isNotBlank();
+    }
+
+    @Test
+    @DisplayName("Spotify Search Parse")
+    void t002() throws Exception {
         MusicSearch musicSearch = new MusicSearch();
-        String out = musicSearch.search("IU");
+        List<Music> out = musicSearch.getSearchResult("IU");
+        System.out.println(out);
+        assertThat(out).isNotEmpty();
     }
 }
