aa757b40c1481efae36a032b86e0c205dfd4fade
parkkhee <khp963@naver.com>
2023-05-30 16:47:46 +0900

    login, logout, html

---
+++ src/main/java/com/ll/tagtune/base/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.ll.tagtune.base.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/usr/member/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(
+                        formLogin -> formLogin
+                                .loginPage("/usr/member/login")
+                                .defaultSuccessUrl("/test")
+
+                )
+                .logout(
+                        logout -> logout
+                                .logoutUrl("/usr/member/logout")
+                                .logoutSuccessUrl("/usr/member/login") // 로그아웃 후 이동할 URL
+                                .invalidateHttpSession(true) // 로그아웃 시 세션 무효화
+                                .deleteCookies("JSESSIONID") // 로그아웃 시 삭제할 쿠키 설정
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/ll/tagtune/boundedContext/member/controller/MemberController.java
@@ -0,0 +1,32 @@
+package com.ll.tagtune.boundedContext.member.controller;
+
+import com.ll.tagtune.base.rq.Rq;
+import com.ll.tagtune.boundedContext.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/usr/member")
+public class MemberController {
+
+    private final MemberService memberService;
+    private final Rq rq;
+
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/login") // 로그인 폼, 로그인 폼 처리는 스프링 시큐리티가 구현, 폼 처리시에 CustomUserDetailsService 가 사용됨
+    public String showLogin() {
+        return "usr/member/loginForm";
+    }
+
+
+    @PostMapping("/logout") // 로그인 폼, 로그인 폼 처리는 스프링 시큐리티가 구현, 폼 처리시에 CustomUserDetailsService 가 사용됨
+    public void logOut() {
+    }
+
+}
