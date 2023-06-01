142c5b9edb2f212fa11f8a06790c43d4756b2eca
Youngwoo Choi <yw823x@daum.net>
2023-06-01 09:24:52 +0900

    Enhancement: last api

--- src/test/java/com/ll/tagtune/base/LastfmTest.java
+++ src/test/java/com/ll/tagtune/base/LastfmTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -12,8 +14,32 @@ class LastfmTest {
     @Test
     @DisplayName("Search Test")
     void t001() throws Exception {
-        String result = SearchEndpoint.musicSearch("아이유");
+        Map result = SearchEndpoint.searchTrack("IU");
         System.out.println(result);
-        assertThat(result).contains("아이유");
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("Get trackInfo Test")
+    void t002() throws Exception {
+        Map result = SearchEndpoint.getTrackInfo("IU", "blueming");
+        System.out.println(result);
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("Get topTags Test")
+    void t003() throws Exception {
+        Map result = SearchEndpoint.getTrackTopTags("IU", "blueming");
+        System.out.println(result);
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    @DisplayName("Recommend From Tag Test")
+    void t004() throws Exception {
+        Map result = SearchEndpoint.getTracksFromTag("k-pop");
+        System.out.println(result);
+        assertThat(result).isNotEmpty();
     }
 }
