e28675a88c5240991ade0d4e02f2cb5b1bb5a70a
Youngwoo Choi <yw823x@daum.net>
2023-06-01 15:03:04 +0900

    Hotfix: entity names and FK errors

--- src/main/java/com/ll/tagtune/boundedContext/music/repository/MusicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ll.tagtune.boundedContext.music.repository;
-
-import com.ll.tagtune.boundedContext.music.entity.Music;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MusicRepository extends JpaRepository<Music, Long> {
-}
--- src/main/java/com/ll/tagtune/boundedContext/music/repository/MusicRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ll.tagtune.boundedContext.music.repository;
-
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class MusicRepositoryImpl implements MusicRepositoryCustom {
-    private final JPAQueryFactory jpaQueryFactory;
-}
