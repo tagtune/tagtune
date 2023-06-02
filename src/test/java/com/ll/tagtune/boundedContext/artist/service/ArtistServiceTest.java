6a10e32d4ee504cb790a84b8155dcee436029a92
Youngwoo Choi <yw823x@daum.net>
2023-06-02 17:31:01 +0900

    Enhancement: artist service

---
+++ src/test/java/com/ll/tagtune/boundedContext/artist/service/ArtistServiceTest.java
@@ -0,0 +1,35 @@
+package com.ll.tagtune.boundedContext.artist.service;
+
+
+import com.ll.tagtune.boundedContext.artist.dto.ArtistDTO;
+import com.ll.tagtune.boundedContext.artist.entity.Artist;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class ArtistServiceTest {
+    @Autowired
+    private ArtistService artistService;
+
+    @Test
+    @DisplayName("Artist Create Test")
+    void t001() {
+        final ArtistDTO artistDTO = ArtistDTO.builder()
+                .artistName("김릘잏게")
+                .build();
+        assertThat(artistService.findByArtistName(artistDTO.getArtistName())).isEmpty();
+        final Artist artist = artistService.create(artistDTO.getArtistName());
+        assertThat(artist.getArtistName()).isEqualTo(artistDTO.getArtistName());
+    }
+}
