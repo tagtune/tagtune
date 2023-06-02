49cfeb11d84f79143ccedf861f1c9699975e4840
parkkhee <khp963@naver.com>
2023-06-02 15:36:33 +0900

    Feat: create LyricRepository.java

---
+++ src/main/java/com/ll/tagtune/boundedContext/lyric/repository/LyricRepository.java
@@ -0,0 +1,10 @@
+package com.ll.tagtune.boundedContext.lyric.repository;
+
+import com.ll.tagtune.boundedContext.lyric.entity.Lyric;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LyricRepository extends JpaRepository<Lyric, Long> {
+    Optional<Lyric> findById(Long id);
+}
