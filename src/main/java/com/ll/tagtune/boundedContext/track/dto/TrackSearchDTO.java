f13444ed7b683c43160b22fb07883c7862b14f3f
Youngwoo Choi <yw823x@daum.net>
2023-06-05 14:51:32 +0900

    Enhancement: Track comments

--- src/main/java/com/ll/tagtune/boundedContext/track/dto/TrackSearchDTO.java
+++ src/main/java/com/ll/tagtune/boundedContext/track/dto/TrackSearchDTO.java
@@ -4,6 +4,11 @@ import com.ll.tagtune.boundedContext.artist.dto.ArtistDTO;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ *  Track 의 title, Artist 를 저장 하는 DTO 입니다.
+ *  <p>
+ *  Track 의 last.fm API 가 Album, Tag 정보를 가져오지 않아 사용합니다
+ */
 @Data
 @Builder
 public class TrackSearchDTO {
--- src/main/java/com/ll/tagtune/boundedContext/track/service/TrackService.java
+++ src/main/java/com/ll/tagtune/boundedContext/track/service/TrackService.java
@@ -36,6 +36,13 @@ public class TrackService {
         return trackRepository.findByTitleAndArtist_ArtistName(title, artistName);
     }
 
+    /**
+     * API 호출 결과에 title, artist 가 제공될 때 Track 을 생성하는 메소드입니다.
+     *
+     * @param title
+     * @param artist
+     * @return Track
+     */
     public Track createTrack(String title, Artist artist) {
         Track track = Track.builder()
                 .title(title)
@@ -47,6 +54,14 @@ public class TrackService {
         return track;
     }
 
+    /**
+     * API 호출 결과에 title, artist, album 이 제공될 때 Track 을 생성하는 메소드입니다.
+     *
+     * @param title
+     * @param artist
+     * @param album
+     * @return Track
+     */
     public Track createTrack(String title, Artist artist, Album album) {
         Track track = Track.builder()
                 .title(title)
@@ -70,6 +85,13 @@ public class TrackService {
         return result;
     }
 
+    /**
+     * 유저가 title, artist 를 알고있을 때 Track 을 리턴하는 메소드입니다.
+     *
+     * @param title
+     * @param artistName
+     * @return RsData Track
+     */
     public RsData<Track> searchTrackFromApi(String title, String artistName) {
         TrackSearchDTO trackDto = ResultParser.searchTracks(title, artistName).stream().findFirst().orElse(null);
 
@@ -78,6 +100,11 @@ public class TrackService {
         return RsData.successOf(getOrCreateTrackByDTO(trackDto));
     }
 
+    /**
+     *  유저가 title 을 알고있을 때 Track 을 리턴하는 메소드입니다.
+     * @param title
+     * @return
+     */
     public RsData<Track> searchTrackFromApi(String title) {
         TrackSearchDTO trackDto = ResultParser.searchTracks(title).stream().findFirst().orElse(null);
 
@@ -86,6 +113,11 @@ public class TrackService {
         return RsData.successOf(getOrCreateTrackByDTO(trackDto));
     }
 
+    /**
+     *  Track 세부 정보가 저장되지 않았을 때 정보를 갱신하는 메소드입니다.
+     * @param track
+     * @return Track with Details
+     */
     public Track getTrackInfo(Track track) {
         TrackInfoDTO trackDto = ResultParser.getTrack(track.getTitle(), track.getArtist().getArtistName());
 
@@ -100,6 +132,14 @@ public class TrackService {
         return updateTrack(track, artist, album);
     }
 
+    /**
+     * TrackSearch 에 적합한 Track 를 생성하거나 리턴합니다.
+     * <p>
+     * API 에서 받아온 Track 를 반드시 받기 위해 사용합니다.
+     *
+     * @param trackSearchDTO
+     * @return Track
+     */
     public Track getOrCreateTrackByDTO(TrackSearchDTO trackSearchDTO) {
         return getTrackByTitleAndArtist(
                 trackSearchDTO.getTitle(),
