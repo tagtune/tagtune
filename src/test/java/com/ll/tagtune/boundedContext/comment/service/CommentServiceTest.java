eea763a14e17d72ef36c4a710b45534ea6854512
parkkhee <khp963@naver.com>
2023-06-06 00:16:21 +0900

    Feat: test about Comment saveComment, modifyComment, deleteComment, showComment

---
+++ src/test/java/com/ll/tagtune/boundedContext/comment/service/CommentServiceTest.java
@@ -0,0 +1,85 @@
+package com.ll.tagtune.boundedContext.comment.service;
+
+import com.ll.tagtune.base.rsData.RsData;
+import com.ll.tagtune.boundedContext.comment.entity.Comment;
+import com.ll.tagtune.boundedContext.comment.repository.CommentRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+@ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class CommentServiceTest {
+
+    @Autowired
+    private CommentService commentService;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    //todo 조회, 삭제, 수정, 좋아요, 대댓글,
+
+    @BeforeEach
+    void beforeEach(){
+        String content = "Hello hello";
+        commentService.saveComment(content, null);
+    }
+
+    @Test
+    @DisplayName("RecommendCnt, DeleteStatus 저장시 기본값 세팅 잘 되는지 test")
+    void t001() throws Exception{
+        List<Comment> allComment = commentRepository.findAll();
+
+        assertThat(allComment).isNotEmpty();
+
+        Optional<Comment> comment = allComment.stream().findFirst();
+        assertThat(comment.isPresent()).isTrue(); // null 체크 추가
+        assertThat(comment.get().getRecommendCnt()).isEqualTo(0);
+        assertThat(comment.get().getDeleteStatus()).isFalse();
+    }
+
+    @Test
+    @DisplayName("댓글 삭제")
+    void t002() throws Exception{
+        List<Comment> allComment = commentRepository.findAll();
+        Optional<Comment> comment = allComment.stream().findFirst();
+
+        commentService.deleteComment(comment.get().getId());
+
+        List<Comment> allComment2 = commentRepository.findAll();
+
+        assertThat(allComment2.stream().count()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("댓글 조회 by id")
+    void t003() throws Exception{
+        Optional<Comment> anyComment = commentRepository.findAll().stream().findAny();
+
+        RsData rsData = commentService.showComment(anyComment.get().getId());
+
+        assertThat(rsData.getData()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("댓글 수정")
+    void t004() throws Exception{
+        Optional<Comment> anyComment = commentRepository.findAll().stream().findAny();
+
+        String content = "abdbdsdfawead";
+        RsData<Comment> comment = commentService.modifyComment(anyComment.get().getId(), content);
+
+        assertThat(comment.getData().getContent()).isEqualTo(content);
+    }
+}
