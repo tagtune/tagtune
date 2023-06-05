919d6904d9273d5ec39ffa227958a28694d7156a
uoise <yw823x@daum.net>
2023-06-05 09:06:03 +0900

    Enhancement: TrackService getOrCreate

--- src/main/java/com/ll/tagtune/boundedContext/track/repository/TrackRepository.java
+++ src/main/java/com/ll/tagtune/boundedContext/track/repository/TrackRepository.java
@@ -3,5 +3,10 @@ package com.ll.tagtune.boundedContext.track.repository;
 import com.ll.tagtune.boundedContext.track.entity.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TrackRepository extends JpaRepository<Track, Long> {
+    Optional<Track> findByTitleAndArtist_Id(String title, Long id);
+
+    Optional<Track> findByTitleAndArtist_ArtistName(String title, String artistName);
 }
