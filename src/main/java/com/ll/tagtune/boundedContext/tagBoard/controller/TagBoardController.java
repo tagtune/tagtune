6e5e5d115834dc1203cacad0ea1fff425c08aa44
Park0720 <ppmm98@naver.com>
2023-06-08 17:24:04 +0900

    Enhancement: Filtering TagBoard

--- src/main/java/com/ll/tagtune/boundedContext/tagBoard/controller/TagBoardController.java
+++ src/main/java/com/ll/tagtune/boundedContext/tagBoard/controller/TagBoardController.java
@@ -1,24 +1,43 @@
 package com.ll.tagtune.boundedContext.tagBoard.controller;
 
+import com.ll.tagtune.boundedContext.tagBoard.entity.TagBoard;
 import com.ll.tagtune.boundedContext.tagBoard.service.TagBoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("usr/category")
 @RequiredArgsConstructor
 public class TagBoardController {
     private final TagBoardService tagBoardService;
+
     @GetMapping("/tagBoard")
-    public String showTagBoard() {
+    public String showTagBoard(Model model, @RequestParam(value = "kw", defaultValue = "") String kw) {
+        List<TagBoard> tagBoardList = tagBoardService.findAll();
+        List<TagBoard> top3TagBoardList = tagBoardService.findTop3ByPopularity(0L);
+        List<TagBoard> filteringTagBoardList = tagBoardService.findByTagBoardNameLike("%" + kw + "%");
+
+        if (!kw.equals("")) {
+            model.addAttribute("tagBoardList", filteringTagBoardList);
+        } else {
+            model.addAttribute("tagBoardList", tagBoardList);
+        }
+        model.addAttribute("top3TagBoardList", top3TagBoardList);
+        model.addAttribute("kw", kw);
         return "usr/category/tagBoard";
     }
 
     @GetMapping("/tag")
-    public String showTag() {
+    public String showTag(Model model, @RequestParam(value = "id") Long id) {
+        TagBoard tagBoard = tagBoardService.findById(id).get();
+        model.addAttribute("tagBoard", tagBoard);
+
         return "usr/category/tag";
     }
-
 }
--- src/main/java/com/ll/tagtune/boundedContext/tagBoard/service/TagBoardService.java
+++ src/main/java/com/ll/tagtune/boundedContext/tagBoard/service/TagBoardService.java
@@ -1,11 +1,14 @@
 package com.ll.tagtune.boundedContext.tagBoard.service;
 
+import com.ll.tagtune.boundedContext.tag.entity.Tag;
+import com.ll.tagtune.boundedContext.tag.repository.TagRepository;
 import com.ll.tagtune.boundedContext.tagBoard.entity.TagBoard;
 import com.ll.tagtune.boundedContext.tagBoard.repository.TagBoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,9 +16,34 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class TagBoardService {
     private final TagBoardRepository tagBoardRepository;
+    private final TagRepository tagRepository;
 
-    @Transactional(readOnly = true)
-    public Optional<TagBoard> findByTagBoard(String tagBoard) {
-        return tagBoardRepository.findByTagBoard(tagBoard);
+    public List<TagBoard> findAll() {
+        return tagBoardRepository.findAll();
+    }
+
+    public List<TagBoard> findTop3ByPopularity(Long popularity) {
+        return tagBoardRepository.findTop3ByPopularity(popularity);
+    }
+
+    public List<TagBoard> findByTagBoardNameLike(String kw) {
+        return tagBoardRepository.findByTagBoardNameLike(kw);
+    }
+
+    public Optional<TagBoard> findById(Long id) {
+        return tagBoardRepository.findById(id);
+    }
+
+    public TagBoard createTagBoard() {
+        TagBoard tagBoard = null;
+        List<Tag> tagList = tagRepository.findAll();
+        for (Tag tag : tagList) {
+            tagBoard = TagBoard.builder()
+                    .tag(tag)
+                    .tagBoardName(tag.getTagName())
+                    .build();
+            tagBoardRepository.save(tagBoard);
+        }
+        return tagBoard;
     }
 }
