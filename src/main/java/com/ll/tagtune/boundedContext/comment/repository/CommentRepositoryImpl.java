97eeceb65d52672b0681ebb3f43b2783623abd84
parkkhee <khp963@naver.com>
2023-06-08 16:12:39 +0900

    Feat: Reply, Comment repository

---
+++ src/main/java/com/ll/tagtune/boundedContext/comment/repository/CommentRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.ll.tagtune.boundedContext.comment.repository;
+
+import com.ll.tagtune.boundedContext.comment.dto.CommentResponseDTO;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.ll.tagtune.boundedContext.comment.entity.QComment.comment;
+
+@RequiredArgsConstructor
+public class CommentRepositoryImpl implements CommentCustomRepository {
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<CommentResponseDTO> getComments(Long trackId) {
+        return queryFactory
+                .select(
+                        Projections.constructor(CommentResponseDTO.class,
+                                comment.id,
+                                comment.deleteStatus,
+                                comment.content,
+                                comment.member.id,
+                                comment.member.username,
+                                comment.replies.size()
+                        )
+                )
+                .from(comment)
+                .where(
+                        comment.track.id.eq(trackId)
+                                .and(
+                                        comment.deleteStatus.eq(false)
+                                                .or(
+                                                        comment.deleteStatus.eq(true)
+                                                                .and(comment.replies.isNotEmpty())
+                                                )
+                                )
+
+                )
+                .orderBy(comment.id.desc())
+                .fetch();
+    }
+}
---
+++ src/main/java/com/ll/tagtune/boundedContext/reply/repository/ReplyRepository.java
@@ -0,0 +1,11 @@
+package com.ll.tagtune.boundedContext.reply.repository;
+
+import com.ll.tagtune.boundedContext.comment.entity.Comment;
+import com.ll.tagtune.boundedContext.reply.entity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+    List<Reply> findByParent(Comment comment);
+}
