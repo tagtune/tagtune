a6b3aebcda3cd2e1f9c2d0d0db1688464256f206
parkkhee <khp963@naver.com>
2023-06-08 16:09:57 +0900

    Refactor: CommentService, CommentServiceTest

--- src/main/java/com/ll/tagtune/boundedContext/comment/service/CommentService.java
+++ src/main/java/com/ll/tagtune/boundedContext/comment/service/CommentService.java
@@ -1,34 +1,29 @@
 package com.ll.tagtune.boundedContext.comment.service;
 
 import com.ll.tagtune.base.rsData.RsData;
-import com.ll.tagtune.boundedContext.album.entity.Album;
-import com.ll.tagtune.boundedContext.album.repository.AlbumRepository;
-import com.ll.tagtune.boundedContext.comment.dto.CommentRequestDTO;
 import com.ll.tagtune.boundedContext.comment.dto.CommentResponseDTO;
 import com.ll.tagtune.boundedContext.comment.entity.Comment;
 import com.ll.tagtune.boundedContext.comment.repository.CommentRepository;
+import com.ll.tagtune.boundedContext.comment.repository.CommentRepositoryImpl;
 import com.ll.tagtune.boundedContext.member.entity.Member;
-import com.ll.tagtune.boundedContext.member.repository.MemberRepository;
-import jakarta.persistence.EntityNotFoundException;
+import com.ll.tagtune.boundedContext.track.entity.Track;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import se.michaelthelin.spotify.exceptions.detailed.NotFoundException;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
-    private final AlbumRepository albumRepository;
-    private final MemberRepository memberRepository;
-    public RsData<Comment> saveComment(String content, Album album){
+    private final CommentRepositoryImpl commentRepositoryImpl;
+
+    public RsData<Comment> saveComment(String content, Track track, final Member member){
         Comment comment = Comment.builder()
-                .deleteStatus(false)
+                .member(member)
                 .content(content)
-                .children(new ArrayList<>())
-                .album(album)
+                .track(track)
                 .build();
 
         commentRepository.save(comment);
@@ -36,42 +31,7 @@ public class CommentService {
         return RsData.of("S-1", "댓글이 정상적으로 등록되었습니다.",comment);
     }
 
-    /**
-     * 대댓글 메서드
-     * */
-    public RsData<Comment> saveReply(Long albumId, CommentRequestDTO commentRequestDTO){
-//        Optional<Member> memberById = memberRepository.findById(commentRequestDTO.getMemberId());
-//        if (memberById.isEmpty()) {
-//            return RsData.of("F-1", "해당되는 id의 회원이 없습니다.");
-//        }
-
-//        Optional<Album> albumById = albumRepository.findById(albumId);
-//        if (albumById.isEmpty()) {
-//            return RsData.of("F-2", "해당되는 id의 앨범이 없습니다.");
-//        }
-
-        Optional<Comment> commentById = commentRepository.findById(commentRequestDTO.getParentId());
-        if (commentById.isEmpty()) {
-            return RsData.of("F-3", "해당되는 부모 id의 댓글이 없습니다.");
-        }
-
-        Comment reply = Comment.builder()
-                .deleteStatus(false)
-                .content(commentRequestDTO.getContent())
-                .parent(commentById.get())
-                .children(new ArrayList<>())
-//                .album(albumById.get())
-//                .member(memberById.get())
-                .build();
-
-        commentRepository.save(reply);
-
-        commentById.get().addChildren(reply);
-
-        return RsData.of("S-1", "대댓글이 정상적으로 등록되었습니다.",reply);
-    }
-
-    public RsData<Comment> modifyComment(Long id, String content) {
+    public RsData<Comment> modifyComment(final Long id, String content) {
         Optional<Comment> commentById = commentRepository.findById(id);
 
         if (commentById.isEmpty()) {
@@ -83,49 +43,51 @@ public class CommentService {
                 .content(content)
                 .build();
 
+        commentRepository.save(modifyComment);
+
         return RsData.of("S-1", "댓글을 성공적으로 수정했습니다.",modifyComment);
+    }
+
+    /**
+     * 댓글이 삭제 상태이면서, 덧글이 하나도 없을떄 DB에서 삭제합니다.
+     * @param comment
+     * @return  댓글 DB 삭제 여부
+     */
+    private Boolean orphanDelete(Comment comment) {
+        if(comment.getDeleteStatus() && comment.getReplies().isEmpty()) {
+            delete(comment);
+            return true;
+        }
+        return false;
+    }
+
+    private void delete(Comment comment){
+        commentRepository.delete(comment);
+    }
+
+    public void runOrphanDelete(Comment comment) {
+        orphanDelete(comment);
     }
 
     /**
      * 대댓글 있을경우 삭제 상태만 변경
      * */
-    public RsData deleteComment(Long id) {
-        Optional<Comment> commentById = commentRepository.findById(id);
-        if (commentById.isEmpty()) {
-            return RsData.of("F-1", "해당되는 id의 댓글이 없습니다.");
-        }
-        Comment comment = commentById.get();
+    public RsData<Void> deleteComment(final Long id, final Long memberId) {
+        Optional<Comment> oComment = commentRepository.findById(id);
+        if (oComment.isEmpty())
+            return RsData.of("F-1", "해당하는 데이터가 없습니다.");
 
-        if (comment.getChildren().size() != 0) {
-            comment.changeDeleteStatus(true);
-        }else {
-            commentRepository.deleteById(getDeletableParentComment(comment).getId());
-        }
+        if (!oComment.get().getMember().getId().equals(memberId))
+            return RsData.of("F-2", "잘못된 접근입니다.");
 
-        return RsData.of("S-1", "댓글을 성공적으로 삭제했습니다.");
+        Comment comment = oComment.get().toBuilder().deleteStatus(true).build();
+        commentRepository.save(comment);
+        orphanDelete(comment);
+
+        return RsData.of("S-1", "댓글이 삭제되었습니다.");
     }
 
-    /**
-     * 부모가 있고, 부모의 자식이 1개(지금 삭제하는 댓글)이고, 부모의 삭제 상태가 TRUE인 댓글이라면 재귀
-     * */
-    private Comment getDeletableParentComment(Comment comment) {
-        Comment parent = comment.getParent();
-        if(parent != null && parent.getChildren().size() == 1 && parent.getDeleteStatus())
-            return getDeletableParentComment(parent);
-        return comment;
-    }
-
-    public RsData showComment(Long id) {
-        Optional<Comment> commentById = commentRepository.findById(id);
-        if (commentById.isEmpty()) {
-            return RsData.of("F-1", "해당되는 id의 댓글이 없습니다.");
-        }
-        Comment comment = commentById.get();
-
-        CommentResponseDTO commentResponseDTO = comment.getDeleteStatus() ?
-                new CommentResponseDTO(comment.getId(), "삭제된 댓글입니다.", comment.getMember()) :
-                new CommentResponseDTO(comment.getId(), comment.getContent(), comment.getMember());
-
-        return RsData.successOf(commentResponseDTO);
+    public List<CommentResponseDTO> getCommentsWithReplies(Track track) {
+        return commentRepositoryImpl.getComments(track.getId());
     }
 }
--- src/test/java/com/ll/tagtune/boundedContext/comment/service/CommentServiceTest.java
+++ src/test/java/com/ll/tagtune/boundedContext/comment/service/CommentServiceTest.java
@@ -1,18 +1,29 @@
 package com.ll.tagtune.boundedContext.comment.service;
 
 import com.ll.tagtune.base.rsData.RsData;
-import com.ll.tagtune.boundedContext.comment.dto.CommentRequestDTO;
+import com.ll.tagtune.boundedContext.artist.entity.Artist;
 import com.ll.tagtune.boundedContext.comment.dto.CommentResponseDTO;
 import com.ll.tagtune.boundedContext.comment.entity.Comment;
 import com.ll.tagtune.boundedContext.comment.repository.CommentRepository;
+import com.ll.tagtune.boundedContext.member.entity.Member;
+import com.ll.tagtune.boundedContext.member.repository.MemberRepository;
+import com.ll.tagtune.boundedContext.member.service.MemberService;
+import com.ll.tagtune.boundedContext.reply.entity.Reply;
+import com.ll.tagtune.boundedContext.reply.repository.ReplyRepository;
+import com.ll.tagtune.boundedContext.reply.service.ReplyService;
+import com.ll.tagtune.boundedContext.track.entity.Track;
+import com.ll.tagtune.boundedContext.track.service.TrackService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,116 +36,116 @@ import static org.junit.jupiter.api.Assertions.*;
 class CommentServiceTest {
     @Autowired
     private CommentService commentService;
-
     @Autowired
     private CommentRepository commentRepository;
+    @Autowired
+    private ReplyService replyService;
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private TrackService trackService;
+    private Member member1;
+    private Member member2;
+    private Track track;
+    private Comment[] comments;
+    private int num=0;
 
     //todo 조회, 삭제, 수정, 좋아요, 대댓글,
 
     @BeforeEach
     void beforeEach(){
-        String content = "Hello hello";
-        commentService.saveComment(content, null);
+        String content = "Hello hello%d".formatted(num);
+        String content2 = "vwerwerv_%d %d";
+
+        member1 = memberService.join("parkkhee_%d".formatted(num),"1234").getData();
+        member2 = memberService.join("pahee_%d".formatted(num),"1234").getData();
+
+        track = trackService.createTrack(content, Artist.builder().build());
+
+        comments = IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> commentService.saveComment(content2.formatted(num, i), track, member1).getData())
+                .toArray(Comment[]::new);
+
+        IntStream.rangeClosed(1, 10)
+                .mapToObj(i -> replyService.saveReply(member1, comments[0].getId(), content2.formatted(num, i)));
+        this.num++;
     }
 
     @Test
     @DisplayName("RecommendCnt, DeleteStatus 저장시 기본값 세팅 잘 되는지 test")
     void t001() throws Exception{
-        List<Comment> allComment = commentRepository.findAll();
-
-        assertThat(allComment).isNotEmpty();
-
-        Optional<Comment> comment = allComment.stream().findFirst();
-        assertThat(comment.isPresent()).isTrue(); // null 체크 추가
-        assertThat(comment.get().getRecommendCnt()).isEqualTo(0);
-        assertThat(comment.get().getDeleteStatus()).isFalse();
+        assertThat(comments).isNotEmpty();
+        Arrays.stream(comments).forEach(comment -> assertThat(comment.getDeleteStatus()).isFalse());
     }
 
     @Test
     @DisplayName("댓글 삭제")
     void t002() throws Exception{
-        List<Comment> allComment = commentRepository.findAll();
-        Optional<Comment> comment = allComment.stream().findFirst();
+        Comment comment = comments[0];
+        commentService.deleteComment(comment.getId(), member1.getId());
 
-        commentService.deleteComment(comment.get().getId());
-
-        List<Comment> allComment2 = commentRepository.findAll();
-
-        assertThat(allComment2.stream().count()).isEqualTo(0);
-    }
-
-    @Test
-    @DisplayName("댓글 조회 by id")
-    void t003() throws Exception{
-        Optional<Comment> anyComment = commentRepository.findAll().stream().findAny();
-
-        RsData rsData = commentService.showComment(anyComment.get().getId());
-
-        assertThat(rsData.getData()).isNotNull();
+        assertThat(commentRepository.findById(comment.getId())).isEmpty();
     }
 
     @Test
     @DisplayName("댓글 수정")
     void t004() throws Exception{
-        Optional<Comment> anyComment = commentRepository.findAll().stream().findAny();
+        Comment comment = comments[0];
 
         String content = "abdbdsdfawead";
-        RsData<Comment> comment = commentService.modifyComment(anyComment.get().getId(), content);
+        RsData<Comment> commentRsData = commentService.modifyComment(comment.getId(), content);
 
-        assertThat(comment.getData().getContent()).isEqualTo(content);
+        assertThat(commentRsData.getData().getContent()).isEqualTo(content);
     }
 
     @Test
-    @DisplayName("부모 댓글 children에 댓글 추가")
+    @DisplayName("부모 댓글에 reply추가")
     void t005() throws Exception{
-        Optional<Comment> anyComment = commentRepository.findAll().stream().findAny();
+        Comment comment = comments[0];
+        int beforeReplySize = comment.getReplies().size();
         String content = "babybaby";
 
-        CommentRequestDTO commentResponseDTO = new CommentRequestDTO();
-        commentResponseDTO.setMemberId(null);
-        commentResponseDTO.setContent(content);
-        commentResponseDTO.setParentId(anyComment.get().getId());
+        RsData<Reply> replyRsData = replyService.saveReply(member1, comment.getId(), content);
 
-        commentService.saveReply(null, commentResponseDTO);
-
-        assertThat(anyComment.get().getChildren().size()).isEqualTo(1);
+        assertThat(comment.getReplies().size()).isEqualTo(beforeReplySize+1);
     }
 
     @Test
     @DisplayName("댓글 삭제시 getDeleteStatus()가 true")
     void t006() throws Exception{
-        Optional<Comment> parentComment = commentRepository.findAll().stream().findAny();
+        Comment parentComment = comments[0];
         String content = "babybaby";
 
-        CommentRequestDTO commentResponseDTO = new CommentRequestDTO();
-        commentResponseDTO.setMemberId(null);
-        commentResponseDTO.setContent(content);
-        commentResponseDTO.setParentId(parentComment.get().getId());
+        replyService.saveReply(member1, parentComment.getId() ,content);
 
-        commentService.saveReply(null, commentResponseDTO);
+        commentService.deleteComment(parentComment.getId(), parentComment.getMember().getId());
 
-        commentService.deleteComment(parentComment.get().getId());
-
-        assertThat(parentComment.get().getDeleteStatus()).isTrue();
-        assertThat(commentRepository.count()).isEqualTo(2);
+        assertThat(parentComment.getDeleteStatus()).isTrue();
+        assertThat(commentRepository.findById(parentComment.getId())).isPresent();
     }
 
     @Test
     @DisplayName("자식, 부모 댓글 둘 다 삭제시")
     void t007() throws Exception{
-        Optional<Comment> parentComment = commentRepository.findAll().stream().findAny();
+        Comment parentComment = comments[0];
         String content = "babybaby";
 
-        CommentRequestDTO commentResponseDTO = new CommentRequestDTO();
-        commentResponseDTO.setMemberId(null);
-        commentResponseDTO.setContent(content);
-        commentResponseDTO.setParentId(parentComment.get().getId());
+        RsData<Reply> replyRsData = replyService.saveReply(member1, parentComment.getId(), content);
 
-        RsData<Comment> commentRsData = commentService.saveReply(null, commentResponseDTO);
+        commentService.deleteComment(parentComment.getId(), parentComment.getMember().getId());
 
-        commentService.deleteComment(parentComment.get().getId());
-        commentService.deleteComment(commentRsData.getData().getId());
+        assertThat(parentComment.getDeleteStatus()).isTrue();
+        assertThat(commentRepository.findById(parentComment.getId())).isPresent();
 
-        assertThat(commentRepository.count()).isEqualTo(0);
+        replyService.deleteReply(replyRsData.getData().getId(), member1.getId());
+
+        assertThat(commentRepository.findById(parentComment.getId())).isEmpty();
+    }
+
+    @Test
+    @DisplayName("댓글, 대댓글 다 보여주기")
+    void t008() throws Exception{
+
+
     }
 }
