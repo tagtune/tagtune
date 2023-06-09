0f9b5dfa1477168560ea085b3ffc3f4db16f07ca
parkkhee <khp963@naver.com>
2023-06-09 18:09:10 +0900

    Feat: create saveReply()

--- src/main/java/com/ll/tagtune/boundedContext/reply/controller/ReplyController.java
+++ src/main/java/com/ll/tagtune/boundedContext/reply/controller/ReplyController.java
@@ -11,15 +11,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class replyController {
+public class ReplyController {
     private final Rq rq;
     private final ReplyService replyService;
     private final CommentService commentService;
@@ -36,6 +36,18 @@ public class replyController {
         redirectAttributes.addFlashAttribute("replies", replyRsdata.getData());
 
         return "redirect:/track/"+ trackId +"/comment";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/track/{trackId}/comment/{parentCommentId}/reply")
+    public String saveReply(
+            @PathVariable Long trackId,
+            @PathVariable Long parentCommentId,
+            String iReply
+    ) {
+        RsData<Reply> replyRsData = replyService.saveReply(rq.getMember(), parentCommentId, iReply);
+
+        return rq.redirectWithMsg("/track/{trackId}/comment", replyRsData);
     }
 
     @PreAuthorize("isAuthenticated()")
