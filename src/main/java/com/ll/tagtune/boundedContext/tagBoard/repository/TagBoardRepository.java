a47b78d505499b8cbac99764538191d6620a45e3
Park0720 <125839778+Park0720@users.noreply.github.com>
2023-06-09 15:13:21 +0900

    Enhancement/32 (#37)
    * Enhancement: Tag initdata
    
    * Enhancement: html apply Thymeleaf
    
    * Enhancement: TagRepository
    
    * Enhancement: Filtering TagBoard
    
    * Enhancement: Top3Filtering TagBoard
    
    * Enhancement: Apply PR
    
    * Chore: spring version down 3.1.0 -> 3.0.7(batch_fetch_size bug)
    
    * Enhancement: wordwrap

--- src/main/java/com/ll/tagtune/boundedContext/tagBoard/repository/TagBoardRepository.java
+++ src/main/java/com/ll/tagtune/boundedContext/tagBoard/repository/TagBoardRepository.java
@@ -3,8 +3,15 @@ package com.ll.tagtune.boundedContext.tagBoard.repository;
 import com.ll.tagtune.boundedContext.tagBoard.entity.TagBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TagBoardRepository extends JpaRepository<TagBoard, Long> {
-    Optional<TagBoard> findByTagBoard(String tagBoard);
+    List<TagBoard> findAll();
+
+    Optional<TagBoard> findById(Long id);
+
+    List<TagBoard> findTop3ByOrderByPopularityDesc();
+
+    List<TagBoard> findByTagBoardNameLike(String kw);
 }
