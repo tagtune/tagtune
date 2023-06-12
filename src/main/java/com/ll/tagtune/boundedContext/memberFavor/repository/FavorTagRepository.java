29d49f3e1a0a67642a231f5164384011cfb095dd
wnstn7671 <77955281+wnstn7671@users.noreply.github.com>
2023-06-12 14:26:48 +0900

    Enhancement/45 (#48)
    * Feat: FavorService(Create,Delete), Favor(list.html), MemberFavorController(Create,Delete)
    
    * Fix: FavorService delete
    
    * Feat: verify addFavorTag
    
    * Edit: MemberFavorController(PreAuthorize)
    
    ---------
    
    Co-authored-by: wnstn7671 <ìwnstn7671@naver.com>

--- src/main/java/com/ll/tagtune/boundedContext/memberFavor/repository/FavorTagRepository.java
+++ src/main/java/com/ll/tagtune/boundedContext/memberFavor/repository/FavorTagRepository.java
@@ -4,7 +4,10 @@ import com.ll.tagtune.boundedContext.memberFavor.entity.FavorTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FavorTagRepository extends JpaRepository<FavorTag, Long> {
     List<FavorTag> findAllByMember_Id(Long memberId);
+
+    Optional<FavorTag> findById(Long id);
 }
--- src/main/java/com/ll/tagtune/boundedContext/memberFavor/service/FavorService.java
+++ src/main/java/com/ll/tagtune/boundedContext/memberFavor/service/FavorService.java
@@ -1,11 +1,11 @@
 package com.ll.tagtune.boundedContext.memberFavor.service;
 
+import com.ll.tagtune.base.rsData.RsData;
 import com.ll.tagtune.boundedContext.album.entity.Album;
 import com.ll.tagtune.boundedContext.artist.entity.Artist;
 import com.ll.tagtune.boundedContext.member.entity.Member;
 import com.ll.tagtune.boundedContext.memberFavor.entity.FavorAlbum;
 import com.ll.tagtune.boundedContext.memberFavor.entity.FavorArtist;
-import com.ll.tagtune.boundedContext.memberFavor.entity.FavorBase;
 import com.ll.tagtune.boundedContext.memberFavor.entity.FavorTag;
 import com.ll.tagtune.boundedContext.memberFavor.repository.FavorAlbumRepository;
 import com.ll.tagtune.boundedContext.memberFavor.repository.FavorArtistRepository;
@@ -15,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -27,13 +26,15 @@ public class FavorService {
     private final FavorTagRepository favorTagRepository;
 
     @Transactional(readOnly = true)
-    public List<FavorBase> getFavorTags(final Long memberId) {
-        List<FavorBase> result = new ArrayList<>();
-        result.addAll(favorArtistRepository.findAllByMember_Id(memberId));
-        result.addAll(favorAlbumRepository.findAllByMember_Id(memberId));
-        result.addAll(favorTagRepository.findAllByMember_Id(memberId));
+    public RsData<FavorTag> getFavorTag(final Long id) {
+        return favorTagRepository.findById(id)
+                .map(RsData::successOf)
+                .orElseGet(() -> RsData.of("F-1", "해당하는 데이터가 없습니다."));
+    }
 
-        return result;
+    @Transactional(readOnly = true)
+    public List<FavorTag> getFavorTags(final Long memberId) {
+        return favorTagRepository.findAllByMember_Id(memberId);
     }
 
     public FavorArtist create(final Member member, final Artist artist) {
@@ -67,5 +68,15 @@ public class FavorService {
         favorTagRepository.save(favorTag);
 
         return favorTag;
+    }
+
+    public RsData<Void> delete(final Member member, final Long id) {
+        RsData<FavorTag> rsData = getFavorTag(id);
+        if (rsData.isFail()) return RsData.of("F-1", "해당하는 선호태그가 없습니다.");
+        if (!rsData.getData().getMember().getId().equals(member.getId())) return RsData.of("F-2", "잘못된 접근입니다.");
+        // 태그의 멤버와 제공된 멤버의 id가 일치하는지 검사
+        favorTagRepository.delete(rsData.getData());
+
+        return RsData.of("S-1", "삭제가 완료되었습니다.");
     }
 }
