747c85191eafd24019e31ecf5da5cb953235cf61
wnstn7671 <wnstn7671@naver.com>
2023-06-12 09:39:50 +0900

    Feat: FavorService(Create,Delete), Favor(list.html), MemberFavorController(Create,Delete)

---
+++ src/main/java/com/ll/tagtune/boundedContext/memberFavor/controller/MemberFavorController.java
@@ -0,0 +1,61 @@
+package com.ll.tagtune.boundedContext.memberFavor.controller;
+
+import com.ll.tagtune.base.rq.Rq;
+import com.ll.tagtune.base.rsData.RsData;
+import com.ll.tagtune.boundedContext.memberFavor.dto.FavorTagDTO;
+import com.ll.tagtune.boundedContext.memberFavor.service.FavorService;
+import com.ll.tagtune.boundedContext.tag.dto.TagDTO;
+import com.ll.tagtune.boundedContext.tag.entity.Tag;
+import com.ll.tagtune.boundedContext.tag.service.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/favor")
+@RequiredArgsConstructor
+public class MemberFavorController {
+    private final FavorService favorService;
+    private final TagService tagService;
+    private final Rq rq;
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<FavorTagDTO> favorTags = favorService.getFavorTags(rq.getMember().getId())
+                .stream()
+                .map(tag -> FavorTagDTO.builder()
+                        .id(tag.getId())
+                        .tagId(tag.getData().getId())
+                        .name(tag.getData().getTagName())
+                        .build()
+                )
+                .toList();
+
+        model.addAttribute("favorTags", favorTags);
+
+        return "usr/favor/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/addTag")
+    public String addFavorTag(TagDTO tagDTO) {
+        Tag tag = tagService.getOrCreateTag(tagDTO.getTagName());
+        favorService.create(rq.getMember(), tag);
+
+        return rq.redirectWithMsg("/favor/list", "태그가 추가되었습니다.");
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @DeleteMapping("/{favorTagId}")
+    public String deleteFavorTag(@PathVariable Long favorTagId) {
+        RsData<Void> rsData = favorService.delete(rq.getMember(), favorTagId);
+        if (rsData.isFail()) return rq.historyBack(rsData);
+
+        return rq.redirectWithMsg("/favor/list", "태그가 삭제되었습니다.");
+    }
+}
